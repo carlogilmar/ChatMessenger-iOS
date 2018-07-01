0ea9f26a279b59b55234549cd33dc80a6111fd36
carlogilmar <carlogilmar12@gmail.com>
2018-06-30 19:56:41 -0500

    Adding my UIView but it doesn't work

--- gameofchats/Controller/LoginController.swift
+++ gameofchats/Controller/LoginController.swift
@@ -9,15 +9,40 @@
 import UIKit
 
 class LoginController: UIViewController {
+   
+    //Creating the view component
+    let inputsContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.translatesAutoresizingMaskIntoConstraints = true
+        return view
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //view.backgroundColor = .blue
         view.backgroundColor = UIColor(r: 61, g: 91, b: 151)
+        
+        view.addSubview(inputsContainerView)
+        setupInputsContainerView()
+
+    }
+    
+    func setupInputsContainerView(){
+        //Add the constrains
+       /*
+        inputsContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        inputsContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        inputsContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: -24).isActive = true
+        inputsContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        */
+        inputsContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        inputsContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        inputsContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -40).isActive = true 
+        inputsContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
     }
     
     override var preferredStatusBarStyle : UIStatusBarStyle {
-        return UIStatusBarStyle.lightContent
+        return .lightContent
     }
     
 }
