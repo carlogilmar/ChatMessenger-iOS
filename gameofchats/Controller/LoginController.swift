6e84d1969f243c97790927c4e87e5949177e9fa8
carlogilmar <carlogilmar12@gmail.com>
2018-06-30 19:37:05 -0500

    Adding the preferredStatusBarStyle

--- gameofchats/Controller/LoginController.swift
+++ gameofchats/Controller/LoginController.swift
@@ -12,7 +12,20 @@ class LoginController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        //view.backgroundColor = .blue
+        view.backgroundColor = UIColor(r: 61, g: 91, b: 151)
+    }
+    
+    override var preferredStatusBarStyle : UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
+    }
+    
+}
+
+extension UIColor {
+    
+    convenience init(r: CGFloat, g: CGFloat, b:CGFloat){
+        self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
     }
     
 }
