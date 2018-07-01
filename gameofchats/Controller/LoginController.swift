8b6e736e83ae6c2b31d43c5ab465a82a9e84c01e
carlogilmar <carlogilmar12@gmail.com>
2018-07-01 00:11:35 -0500

    Button for register is done

--- gameofchats/Controller/LoginController.swift
+++ gameofchats/Controller/LoginController.swift
@@ -26,6 +26,7 @@ class LoginController: UIViewController {
         button.backgroundColor = UIColor(r: 80, g: 101, b: 161)
         button.setTitle("Register!", for: .normal)
         button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitleColor(UIColor.white, for: .normal)
         return button
     }()
     
