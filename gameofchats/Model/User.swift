08e02c7a84174eba7a1a946a149ac2d159ad1035
carlogilmar <carlogilmar12@gmail.com>
2018-07-03 01:19:28 -0500

    Adding User model

---
+++ gameofchats/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  gameofchats
+//
+//  Created by Carlo Gilmar on 03/07/18.
+//  Copyright © 2018 Carlo Gilmar. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+    var name: String?
+    var email: String?
+}
