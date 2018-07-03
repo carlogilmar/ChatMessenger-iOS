78e6800f0e26d286630e2720a93e16a7fc6547f8
carlogilmar <carlogilmar12@gmail.com>
2018-07-03 01:10:43 -0500

    Initialize table view with five rows

--- gameofchats/Controller/NewMessageController.swift
+++ gameofchats/Controller/NewMessageController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class NewMessageController: UITableViewController {
+    
+    let cellId = "cellId"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,5 +19,15 @@ class NewMessageController: UITableViewController {
 
     @objc func dismissNewMessageController(){
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellId)
+        cell.textLabel?.text = "Dummy Text!"
+        return cell
     }
 }
