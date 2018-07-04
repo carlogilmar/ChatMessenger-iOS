af8c208e6f74ed6dafaa26b4930251b17dccf08d
carlogilmar <carlogilmar12@gmail.com>
2018-07-03 22:29:14 -0500

    Adding validation for save a image for be the imageProfile

--- gameofchats/Controller/LoginController+handlers.swift
+++ gameofchats/Controller/LoginController+handlers.swift
@@ -8,11 +8,35 @@
 
 import UIKit
 
-extension LoginController {
+extension LoginController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @objc func handleSelectProfileImageView(){
         print("Hello there!")
         let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
         present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        print("Canceled picker!")
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        print(info)
+        
+        var selectedImageFromPicker: UIImage?
+        
+        if let editedImage = info["UIImagePickerControllerEditedImage"] as? UIImage {
+            selectedImageFromPicker = editedImage
+        } else if let originalImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            selectedImageFromPicker = originalImage
+        }
+        
+        if let selectedImage = selectedImageFromPicker {
+            profileImageView.image = selectedImage
+        }
+        
+        dismiss(animated: true, completion: nil)
     }
 }
