428727557ff02aa68b9242a079ce8810fa402e67
carlogilmar <carlogilmar12@gmail.com>
2018-07-05 21:16:12 -0500

    Upload the select image to firebase storaga

--- gameofchats/Controller/LoginController+handlers.swift
+++ gameofchats/Controller/LoginController+handlers.swift
@@ -7,8 +7,61 @@
 //
 
 import UIKit
+import Firebase
 
 extension LoginController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @objc func handleRegister() {
+        guard emailTextField.text != "" , passwordTextField.text != "", nameTextField.text != ""
+            else {
+                let invalidFormAlert = UIAlertController(title: " Register FAIL!!!", message: "Llena los campos solicitados 😎", preferredStyle: UIAlertControllerStyle.alert)
+                invalidFormAlert.addAction(UIAlertAction(title: "Va de nuez!", style: UIAlertActionStyle.default, handler: nil))
+                self.present(invalidFormAlert, animated: true, completion: nil)
+                print("Form not valid!")
+                return
+        }
+        
+
+
+        Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            
+            if error != nil {
+                print(error!)
+                return
+            }
+            
+            let uuid = Auth.auth().currentUser?.uid
+            
+            let ref = Database.database().reference(fromURL: "https://gameofchats-db1b4.firebaseio.com/")
+            let usersReference = ref.child("users").child(uuid!)
+            let values: Dictionary = ["email": self.emailTextField.text!,
+                                      "password": self.passwordTextField.text!,
+                                      "name": self.nameTextField.text!]
+            
+            usersReference.updateChildValues(values, withCompletionBlock: { (err, ref) in
+                if err != nil {
+                    print(err!)
+                    return
+                }
+                print("Thats ok!!!>>>>>>>>>>>>>>> \(uuid!)")
+                let storageRef = Storage.storage().reference().child("myImage.png")
+                let uploadData = UIImagePNGRepresentation(self.profileImageView.image!)
+                // Upload the file to the path "images/rivers.jpg"
+                let uploadTask = storageRef.putData(uploadData!, metadata: nil) { (metadata, error) in
+                    if error != nil {
+                        print(error)
+                        print("Image saved no saved!!!")
+                        return
+                    }
+                    print("Image saved!!!")
+                    print(metadata)
+                }
+                self.dismiss(animated: true, completion: nil)
+            })
+            
+            
+        }
+    }
     
     @objc func handleSelectProfileImageView(){
         print("Hello there!")
